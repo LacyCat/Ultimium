69f05800335ffcafcf0b6b439befe0580ab016cc
LacyCat <166228777+LacyCat@users.noreply.github.com>
2026-01-08 14:50:35 +0900

    학원가기전 작업

--- src/main/java/cat/Lacycat/ultimium/Feature/HardCoreManager.java
+++ src/main/java/cat/Lacycat/ultimium/Feature/HardCoreManager.java
@@ -1,9 +1,27 @@
 package cat.Lacycat.ultimium.Feature;
 
-public class HardCoreManager {
-    private static int Hard = 0;
-    public static void add(int n) { Hard += n; }
-    public static void subtract(int n) { Hard -= n; }
-    public static int get() { return Hard; }
+import cat.Lacycat.ultimium.Feature.Curse.Curse;
+import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class HardCoreManager {
+    private JavaPlugin plugin;
+    public HardCoreManager(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    private int Hard = 0;
+    private List<Curse> curses = new ArrayList<>();
+    public void add(int n) { Hard += n; }
+    public void subtract(int n) { Hard -= n; }
+    public int get() { return Hard; }
+    public void registerCurse(Curse c) { curses.add(c); }
+    public void unregisterCurse(Curse c) { curses.remove(c); }
+    public void initCurse() {
+        for (Curse i : curses) {
+            i.register(plugin);
+        }
+    }
 }
--- src/main/java/cat/Lacycat/ultimium/Ultimium.java
+++ src/main/java/cat/Lacycat/ultimium/Ultimium.java
@@ -1,13 +1,31 @@
 package cat.Lacycat.ultimium;
 
+import cat.Lacycat.ultimium.Feature.Curse.KeepInventoryCurse;
+import cat.Lacycat.ultimium.Feature.HardCoreManager;
+import cat.Lacycat.ultimium.Feature.Listener.BlockBreakListener;
+import cat.Lacycat.ultimium.Feature.Listener.EntityDeathListener;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.units.qual.K;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public final class Ultimium extends JavaPlugin {
+    public HardCoreManager hcm = new HardCoreManager(this);
+    public List<Listener> alltriggers = new ArrayList<>();
 
     @Override
     public void onEnable() {
+        alltriggers.add(new BlockBreakListener(hcm));
+        alltriggers.add(new EntityDeathListener(hcm));
+        for (Listener i : alltriggers) {
+            Bukkit.getPluginManager().registerEvents(i,this);
+        }
         // Plugin startup logic
-
+        hcm.registerCurse(new KeepInventoryCurse());
+        hcm.initCurse();
     }
 
     @Override
